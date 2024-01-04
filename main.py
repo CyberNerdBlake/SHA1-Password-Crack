f64bbc88e39e6e926d51715336b6e7328af39606
Blake <108043930+CyberNerdBlake@users.noreply.github.com>
2024-01-03 20:25:43 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+import hashlib
+from colorama import Fore
+from colorama import init
+init()
+LOGO = f"""{Fore.RED}
+
+                          _____                          
+                   _.+sd$$$$$$$$$bs+._                   
+               .+d$$$$$$$$$$$$$$$$$$$$$b+.               
+            .sd$$$$$$$P^*^T$$$P^*"*^T$$$$$bs.            
+          .s$$$$$$$$P*     `*' _._  `T$$$$$$$s.          
+        .s$$$$$$$$$P          ` :$;   T$$$$$$$$s.        
+       s$$$$$$$$$$;  db..+s.   `**'    T$$$$$$$$$s       
+     .$$$$$$$$$$$$'  `T$P*'             T$$$$$$$$$$.     
+    .$$$$$$$$$$$$P                       T$$$$$$$$$$.    
+   .$$$$$$$$$$$$$b                       `$$$$$$$$$$$.   
+  :$$$$$$$$$$$$$$$.                       T$$$$$$$$$$$;  
+  $$$$$$$$$P^*' :$$b.                     d$$$$$$$$$$$$  
+ :$$$$$$$P'      T$$$$bs._               :P'`*^T$$$$$$$; 
+ $$$$$$$P         `*T$$$$$b              '      `T$$$$$$ 
+:$$$$$$$b            `*T$$$s                      :$$$$$;
+:$$$$$$$$b.                                        $$$$$;
+$$$$$$$$$$$b.                                     :$$$$$$
+$$$$$$$$$$$$$bs.                                 .$$$$$$$
+$$$$$$$$$$$$$$$$$bs.                           .d$$$$$$$$
+:$$$$$$$$$$$$$P*"*T$$bs,._                  .sd$$$$$$$$$;
+:$$$$$$$$$$$$P     TP^**T$bss++.._____..++sd$$$$$$$$$$$$;
+ $$$$$$$$$$$$b           `T$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 
+ :$$$$$$$$$$$$b.           `*T$$P^*"*"*^^*T$$$$$$$$$$$$; 
+  $$$b       `T$b+                        :$$$$$$$BUG$$  
+  :$P'         `"'               ,._.     ;$$$$$$$$$$$;  
+   \                            `*TP*     d$$P*******$   
+    \                                    :$$P'      /    
+     \                                  :dP'       /     
+      `.                               d$P       .'      
+        `.                             `'      .'        
+          `-.                               .-'          
+             `-.                         .-'             
+                `*+-._             _.-+*'                
+                      `"*-------*"'{Fore.RESET}"""
+print(LOGO)
+
+def converted_text_to_sha1(text):
+    digest = hashlib.sha1(
+        text.encode()
+    ).hexdigest()
+    return digest
+
+print(f"{Fore.RED}Enter SHA1 to Crack: {Fore.RED}", end="")
+user_sha1 = input()
+cleaned_user_sha1 = user_sha1.strip().lower()
+print(Fore.RESET)
+with open('./passwords.txt') as a:
+    for line in a:
+        password = line.strip()
+        converted_sha1 = converted_text_to_sha1(
+            password)
+        if cleaned_user_sha1 == converted_sha1:
+            print(f"{Fore.RED}Password found: {password}")
+            break
+
+input()
